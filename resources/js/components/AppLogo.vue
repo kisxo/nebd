f513ad1f86810fa4f207013ebedfe3fe283f4daf
uday <udaysubba2004@gmail.com>
2025-09-28 15:26:17 +0530

    refactor: update styles and structure for AppLogo and InfoBar components; add responsive design and new InfoBar component

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,14 +3,11 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
-    <div
-        class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground"
-    >
-        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
+    <div class="flex size-15 items-center justify-center rounded-md ">
+        <AppLogoIcon class="size-8" />
     </div>
-    <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate leading-tight font-semibold"
-            >Laravel Starter Kit</span
-        >
+    <div class="ml-1 grid flex-1 text-center text-sm">
+        <span class="mb-0.5 truncate leading-tight font-semibold">STEP UP DANCE ACADEMY</span>
+        <span class="mb-0.5 truncate leading-tight font-thin">Presents</span>
     </div>
 </template>
---
+++ resources/js/components/InfoBar.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="infobar flex flex-col md:flex-row gap-2 w-full bg-neutral-900 flex justify-between p-3 caption md:small font-bold fill-white text-neutral-200 align-center">
+        <div class="left-infobar flex align-center">
+            <div class="flex">
+                <span>
+                    <svg aria-hidden="true" class="size-4 mr-1" viewBox="0 0 384 512" xmlns="http://www.w3.org/2000/svg"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg>
+                </span>
+                <span >Sarupathar, Golaghat - 785621 (ASSAM)</span>
+            </div>
+        </div>
+        <div class="right-infobar self-end">
+            <div class="flex gap-4">
+                <div class="flex">
+                    <span>
+                        <svg aria-hidden="true" class="size-4 mr-1" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"></path></svg>
+                    </span>
+                    <span>70862 08757</span>
+                </div>
+                <div class="flex">
+                    <a href="mailto:info@nebd.in" class="flex align-center">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="size-4 mr-1" viewBox="0 0 16 16"><path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/></svg>
+                        </span>
+                        <span>info@nebd.in</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
